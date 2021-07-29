a3dbc53026b8d97944d83b442b5bc9960d0d6085
felipesouza16 <fesouza2345@gmail.com>
2021-07-29 13:58:23 -0300

    mudanças

--- atvjs/index.js
+++ atvjs/index.js
@@ -8,20 +8,26 @@ function salvar(){
     let pais = (document.getElementById("pais").value);
     marca = {'id':id, 'nome':nome, 'desc':desc, 'pais':pais};
     marcas.push(marca);
+    localStorage.setItem("marcas", JSON.stringify(marcas))
     document.getElementById("id").value = '';
     document.getElementById("nome").value = '';
     document.getElementById("desc").value = '';
     document.getElementById("pais").value = '';
-    mostrarlista();
+}
+function capturarStorage(){
+    let seila = JSON.parse(localStorage.getItem("marcas"));
+    if(seila != null){
+        marcas = seila;
+    }
 }
 function mostrarlista(){
-    let tabela = document.getElementById('tabela-marcas');
+    let tabela = document.getElementById('tabela-marcas'); 
     let corpo = tabela.getElementsByTagName('tbody')[0];
     corpo.innerHTML = "";
     marcas.forEach(m => {
-        corpo.innerHTML += `<tr><td>${m['id']}</td> <td>${m['nome']}</td> <td>${m['desc']}</td> <td>${m['pais']}</td></tr>` 
-    });
+         corpo.innerHTML += `<tr><td>${m['id']}</td> <td>${m['nome']}</td> <td>${m['desc']}</td> <td>${m['pais']}</td></tr>` 
+     });
 }
 window.onload = function(){
-    mostrarlista();
+    capturarStorage();
 }
