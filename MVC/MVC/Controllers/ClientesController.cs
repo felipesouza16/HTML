253bc4046edd015e54393d1436354dfa3360ed8f
felipesouza16 <fesouza2345@gmail.com>
2021-08-06 15:58:31 -0300

    MVCS

---
+++ MVC/MVC/Controllers/ClientesController.cs
@@ -0,0 +1,51 @@
+﻿using MVC.Models;
+using MVC.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class ClientesController : Controller
+    {
+        ClientesRepository repository = new ClientesRepository();
+        // GET: Clientes
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Cadastro(Clientes model)
+        {
+            repository.Create(model);
+            return View();
+        }
+        public ActionResult Listar()
+        {
+            return View(repository.Read());
+        }
+        public ActionResult Deletar(int id)
+        {
+            repository.Delete(id);
+            return RedirectToAction("Listar");
+        }
+        [HttpPost]
+        public ActionResult Editar(Clientes model)
+        {
+            repository.Update(model);
+            return RedirectToAction("Listar");
+        }
+        [HttpGet]
+        public ActionResult Editar(int id)
+        {
+            return View(repository.Read(id));
+        }
+    }
+}
---
+++ MVC/MVC/Repository/ClientesRepository.cs
@@ -0,0 +1,44 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Repository
+{
+    public class ClientesRepository
+    {
+        private static List<Clientes> clientes = new List<Clientes>();
+        static int id = 0;
+        public void Create(Clientes model)
+        {
+            model.Id = id;
+            id++;
+            clientes.Add(model);
+        }
+        public Clientes Read(int id)
+        {
+            return clientes.Find(c => c.Id == id);
+        }
+        public List<Clientes> Read()
+        {
+            return clientes;
+        }
+        public void Update(Clientes model)
+        {
+            int index = clientes.FindIndex(v => v.Id == model.Id);
+            if (index != -1)
+            {
+                clientes[index] = model;
+            }
+        }
+        public void Delete(int id)
+        {
+            Clientes model = Read(id);
+            if (model != null)
+            {
+               clientes.Remove(model);
+            }
+        }
+    }
+}
